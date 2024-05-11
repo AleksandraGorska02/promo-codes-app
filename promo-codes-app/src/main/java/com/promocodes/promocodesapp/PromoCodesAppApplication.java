27f510988a50bcefb5498da7ed6e10878e1785e2
Aleksandra Gorska <aleksandra.gorska02@gmail.com>
2024-05-11 12:40:50 +0200

    start

---
+++ promo-codes-app/src/main/java/com/promocodes/promocodesapp/PromoCodesAppApplication.java
@@ -0,0 +1,13 @@
+package com.promocodes.promocodesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoCodesAppApplication.class, args);
+    }
+
+}
---
+++ promo-codes-app/src/test/java/com/promocodes/promocodesapp/PromoCodesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promocodes.promocodesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoCodesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
